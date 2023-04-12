5a49bd8a1f1bc0d2990be827e12690792a862e5f
Nathan Kutzan <nkutzan@gmail.com>
2023-04-12 17:54:46 -0400

    remove debug

--- gpio/index.js
+++ gpio/index.js
@@ -4,7 +4,6 @@ const http = require('http');
 const conn = new Gpio(17, 'in');
 
 http.createServer(function (req, res) {
-    console.log(conn.readSync())
     res.setHeader("Content-Type", "application/json")
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.write((conn.readSync() === 1).toString());
