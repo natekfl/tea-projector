2bab4b6369e89ea57ce6dd56cd34ad1f3a7b1f73
Nathan Kutzan <nkutzan@gmail.com>
2023-04-15 23:35:55 -0400

    add override

--- gpio/index.js
+++ gpio/index.js
@@ -1,11 +1,19 @@
 const Gpio = require('onoff').Gpio;
-const http = require('http'); 
+const http = require('http');
+
+let overriden = false
 
 const conn = new Gpio(17, 'in');
 
 http.createServer(function (req, res) {
-    res.setHeader("Content-Type", "application/json")
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.write((conn.readSync() === 1).toString());
-    res.end();
-  }).listen(3001);
+  res.setHeader("Content-Type", "application/json")
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.write(((conn.readSync() === 1) || overriden).toString());
+  res.end();
+}).listen(3001);
+
+setInterval(() => {
+  fetch(`http://cvcoc.nathankutzan.info/gridoverride/status`)
+    .then((response) => response.json())
+    .then((data) => overriden = data)
+}, 1000)
